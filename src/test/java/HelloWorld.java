9984dbffa6e6f88202bc7e91af954ddb83267197
miniGIT2021 <90119195+miniGIT2021@users.noreply.github.com>
2021-09-19 09:02:07 +0530

    Update HelloWorld.java
    rebase demo

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is demo for github");
+        System.out.println("This is demo for rebase from intellij");
     }
 }
