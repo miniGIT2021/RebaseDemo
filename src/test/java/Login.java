29f4d89d369ce473fb46b39b5c6cd523ad60cae2
Minisha Moudgil <minishamoudgil20@gmail.com>
2021-09-20 18:10:45 +0530

    Adding log statement

---
+++ src/test/java/Login.java
@@ -0,0 +1,8 @@
+import java.util.logging.Logger;
+
+public class Login {
+    public static void main(String[] args) {
+    Logger log = Logger.getLogger("login");
+        log.info("This is my log info");
+    }
+}
