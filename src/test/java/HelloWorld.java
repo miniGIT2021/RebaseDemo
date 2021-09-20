bc423627eb7ad060c1ccc40cb697403c6a8b632f
miniGIT2021 <90119195+miniGIT2021@users.noreply.github.com>
2021-09-20 17:57:52 +0530

    Update HelloWorld.java
    Fixing the broken build

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args)s {
+    public static void main(String[] args) {
         System.out.println("This is demo for github");
         System.out.println("This is demo for rebase from intellij");
     }
